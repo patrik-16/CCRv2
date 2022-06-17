9bf18db10f72fb4a7bc63abd3ba4aebe8501652d
Patrik <firzen@mailbox.org>
2022-06-17 19:18:21 +0200

    First Commit

---
+++ CCRv2/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+// import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <Header/>
+
+  <main>
+    <TheWelcome/>
+    <HelloWorld/>
+
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
